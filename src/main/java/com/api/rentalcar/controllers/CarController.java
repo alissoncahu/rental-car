a66292be1fe6d99a9951d80e29a55784822aa158
Alisson Barbosa <alisson.barbosa@mv.com.br>
2024-03-02 11:08:11 -0300

    Criação de endpoints de delete e update

--- src/main/java/com/api/rentalcar/controllers/CarController.java
+++ src/main/java/com/api/rentalcar/controllers/CarController.java
@@ -3,6 +3,7 @@ package com.api.rentalcar.controllers;
 import com.api.rentalcar.domain.car.Car;
 import com.api.rentalcar.domain.user.User;
 import com.api.rentalcar.dtos.CarDTO;
+import com.api.rentalcar.repositories.CarRepository;
 import com.api.rentalcar.services.CarService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController()
 @RequestMapping("/cars")
@@ -20,26 +22,57 @@ public class CarController {
     @Autowired
     private CarService carService;
 
+    @Autowired
+    private CarRepository repository;
+
     @PostMapping
     public ResponseEntity<Car> createCar(@Valid @RequestBody CarDTO car){
-
         Car newCar = carService.createCar(car);
         return new ResponseEntity<>(newCar, HttpStatus.CREATED);
-
     }
 
     @GetMapping
     public ResponseEntity<List<Car>> getAllCars(){
         List<Car> cars = this.carService.getAllCars();
         return new ResponseEntity<>(cars, HttpStatus.OK);
+    }
 
+    @GetMapping(path = "/{licensePlateCar}")
+    public ResponseEntity<Car> getCarByLicensePlateCar(@PathVariable String licensePlateCar) throws Exception {
+        Car car = this.carService.findCarByLicensePlateCar(licensePlateCar);
+        return new ResponseEntity<>(car, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Car> getCarById(@PathVariable Long id) throws Exception {
+    public ResponseEntity<Car> getCarByLicensePlateCar(@PathVariable Long id) throws Exception {
         Car car = this.carService.findCarById(id);
         return new ResponseEntity<>(car, HttpStatus.OK);
-
     }
 
+    @DeleteMapping(path = "/{licensePlateCar}")
+    public ResponseEntity<Object> deleteCarByLicensePlateCar(@PathVariable String licensePlateCar) throws Exception {
+        Car car = this.carService.findCarByLicensePlateCar(licensePlateCar);
+        this.carService.delete(car);
+        return ResponseEntity.status(HttpStatus.OK).body("Carro excluído com sucesso");
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Object> deleteCarById(@PathVariable Long id) throws Exception {
+        Car car = this.carService.findCarById(id);
+        this.carService.delete(car);
+        return ResponseEntity.status(HttpStatus.OK).body("Carro excluído com sucesso");
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Car> updateCar(@PathVariable Long id, @RequestBody @Valid CarDTO carDto) throws Exception {
+        Car car = this.carService.findCarById(id);
+
+        car.setBrandCar(carDto.brandCar());
+        car.setColorCar(carDto.colorCar());
+        car.setModelCar(carDto.modelCar());
+        car.setLicensePlateCar(carDto.licensePlateCar());
+
+        this.repository.save(car);
+        return new ResponseEntity<>(car, HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/api/rentalcar/controllers/UserController.java
+++ src/main/java/com/api/rentalcar/controllers/UserController.java
@@ -1,9 +1,10 @@
 package com.api.rentalcar.controllers;
 
-import com.api.rentalcar.domain.car.Car;
 import com.api.rentalcar.domain.user.User;
 import com.api.rentalcar.dtos.UserDTO;
+import com.api.rentalcar.repositories.UserRepository;
 import com.api.rentalcar.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,26 +19,57 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository repository;
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDTO user){
-
         User newUser = userService.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
-
     }
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> users = this.userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
 
+    @GetMapping(path = "/{cpf}")
+    public ResponseEntity<User> getUserByCpf(@PathVariable String cpf) throws Exception {
+        User user = this.userService.getUserByCpf(cpf);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) throws Exception {
-        User user = this.userService.findUserById(id);
+        User user = this.userService.getUserById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
-
     }
 
+    @DeleteMapping(path = "/{cpf}")
+    public ResponseEntity<Object> deleteUserByCpf(@PathVariable String cpf) throws Exception {
+        User user = this.userService.getUserByCpf(cpf);
+        this.userService.delete(user);
+        return ResponseEntity.status(HttpStatus.OK).body("Usuário excluído com sucesso");
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Object> deleteUserById(@PathVariable Long id) throws Exception {
+        User user = this.userService.getUserById(id);
+        this.userService.delete(user);
+        return ResponseEntity.status(HttpStatus.OK).body("Usuário excluído com sucesso");
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody @Valid UserDTO userDto) throws Exception {
+        User user = this.userService.getUserById(id);
+
+        user.setFirstName(userDto.firstName());
+        user.setLastName(userDto.lastName());
+        user.setCpf(userDto.cpf());
+        user.setEmail(userDto.email());
+
+        this.repository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.ACCEPTED);
+    }
 }
